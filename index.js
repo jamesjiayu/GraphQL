234d232060193df0a28add2d8139cb30cad51c4d
jamesjiayu <jamesjiayu@users.noreply.github.com>
2023-06-29 15:52:05 -0500

    1 file modified

--- index.js
+++ index.js
@@ -4,16 +4,36 @@ var { buildSchema } = require('graphql');
 
 // Construct a schema, using GraphQL schema language
 var schema = buildSchema(`
+
+type Account{
+  name: String,
+  age:Int,
+  location: String,
+}
   type Query {
-    hello: String
+    hello: String,
+    getName: String,
+    getAge: Int,
+    getAllNames:[String],
+    getAllAges:[Int],
+    getAccountInfo: Account,
   }
 `);
 
 // The root provides a resolver function for each API endpoint
 var root = {
   hello: () => {
-    return 'Hello, world!';
-  }
+    return 'Hello, world!!!';
+  },
+  getName: () => 'getName: Jim',
+  getAge() {
+    return 18;
+  },
+  getAllNames: () => ['JJ', 'MM', 'CC'],
+  getAllAges: () => [18, 19, 99],
+  getAccountInfo: () => {
+    return { name: 'Jim', age: 18, location: 'Dallas, TX' };
+  },
 };
 
 var app = express();
@@ -22,7 +42,7 @@ app.use(
   graphqlHTTP({
     schema: schema,
     rootValue: root,
-    graphiql: true
+    graphiql: true,
   })
 );
 app.listen(4000);
